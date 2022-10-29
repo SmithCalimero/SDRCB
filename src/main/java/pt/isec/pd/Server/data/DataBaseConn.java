4fe8ff604d60b97882f791cb1fe831185d02f636
eduardo <eduardobento5674@gmail.com>
2022-10-29 22:59:29 +0100

    files updated

--- src/main/java/pt/isec/pd/Server/data/DataBaseConn.java
+++ src/main/java/pt/isec/pd/Server/data/DataBaseConn.java
@@ -3,12 +3,11 @@ package pt.isec.pd.Server.data;
 import java.sql.*;
 
 public class DataBaseConn {
-    private  String DATABASE_URL = "jdbc:sqlite:databases/db2.db";
+    private  String DATABASE_URL = "jdbc:sqlite:";
     private final Connection dbConn;
 
     public DataBaseConn(String path) throws SQLException {
-        //DATABASE_URL += path;
-        //System.out.println(DATABASE_URL);
+        DATABASE_URL += path;
         dbConn = DriverManager.getConnection(DATABASE_URL);
     }
 
