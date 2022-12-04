b37de6fc8c25ee03fb554aebdb6dbaa97da98fee
eduardo <eduardobento5674@gmail.com>
2022-12-04 18:33:03 +0000

    fix comparator

--- src/main/java/pt/isec/pd/shared_data/CompareDbVersionHeartBeat.java
+++ src/main/java/pt/isec/pd/shared_data/CompareDbVersionHeartBeat.java
@@ -5,6 +5,11 @@ import java.util.Comparator;
 public class CompareDbVersionHeartBeat implements Comparator<HeartBeat> {
     @Override
     public int compare(HeartBeat o1, HeartBeat o2) {
-        return Integer.compare(o1.getDbVersion(),o2.getDbVersion());
+        int firstComp = Integer.compare(o1.getDbVersion(),o2.getDbVersion());
+        if(firstComp != 0) {
+            return firstComp;
+        }
+
+        return -Integer.compare(o1.getActiveConnections(),o2.getActiveConnections());
     }
 }
